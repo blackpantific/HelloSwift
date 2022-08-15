19ae2880c5a126cc9cf0adf44ecf214e78bb2b4a
BlackPantific <blackpantific@gmail.com>
2022-08-15 15:27:33 +0300

    collections

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -4,25 +4,50 @@
 //
 //  Created by Алексей Вовченко on 08.08.2022.
 
+var array = [Int]()
+var someArray: [Int] = [1,2,3,4]
+var shoppingList: [String] = ["Eggs", "Milk"]
+shoppingList.append("Flour")
+shoppingList += ["Baking Powder"]
 
-//оператор замкнутого диапазона
-for index in 1...5 {
- print("\(index) умножить на 5 будет \(index * 5)")
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+
+shoppingList[4...6] = ["Bananas", "Apples"]
+
+for i in shoppingList{
+    print(i)
 }
 
+shoppingList.insert("Maple Syrup", at: 0)
 
-let names = ["Anna", "Alex", "Brian", "Jack"]
-let count = names.count
-
-//оператор полузамкнутого диапазона
-for i in 0..<count {
- print("Person \(i + 1) будет \(names[i])")
+for i in shoppingList{
+    print(i)
 }
 
-for name in names[2...] {
-    print(name)
+let mapleSyrup = shoppingList.remove(at: 0)
+
+for (index, value) in shoppingList.enumerated() {
+  print("Item \(index + 1): \(value)")
 }
 
-let threeMoreDoubleQuotationMarks = #"""
-Here are three more double quotes: """
-"""#
+var letters = Set<Character>()
+print("letters имеет тип Set<Character> с \(letters.count) элементами.")
+
+letters.insert("a")
+letters.insert("a")
+
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+
+if let removedGenre = favoriteGenres.remove("Rock") {
+    print("\(removedGenre)? С меня хватит.")
+} else {
+    print("Меня это не сильно заботит.")
+}
+// Выведет "Rock? С меня хватит."
+
+
+var namesOfIntegers = [Int: String]()
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+var res = airports["YYZ"]
